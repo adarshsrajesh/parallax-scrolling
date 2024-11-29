daeaddedcb8a7d3dd47abb2d607a4290867c700d
adarshsrajesh <adarshsrajesh@gmail.com>
2024-11-29 14:39:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let avatar = document.getElementById('avatarimage')
+let quote =document.getElementById('quote')
+let icon = document.getElementById('iconimage')
+
+
+window.addEventListener('scroll' ,() => {
+
+    let a = window.scrollY
+
+
+    avatar.style.top = a*(.3)+'px'
+    quote.style.top = a*(.3)+'px'
+    icon.style.left=a*(-.1)+'px' 
+    
+   
+
+})
