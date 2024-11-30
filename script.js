5c32a30df7f4dbbbdc3b1c81340a79fbe16d0fce
adarshsrajesh <adarshsrajesh@gmail.com>
2024-11-30 22:20:28 +0530

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let avatar = document.getElementById('avatarimage')
 let quote =document.getElementById('quote')
 let icon = document.getElementById('iconimage')
+let smoke = document.getElementById('smokeimage')
+
 
 
 window.addEventListener('scroll' ,() => {
@@ -10,7 +12,8 @@ window.addEventListener('scroll' ,() => {
 
     avatar.style.top = a*(.3)+'px'
     quote.style.top = a*(.3)+'px'
-    icon.style.left=a*(-.1)+'px' 
+    icon.style.left=a*(-.1)+'px'
+    smoke.style.right = a*(-.1)+'px'
     
    
 
